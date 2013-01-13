1b0bbb5c0c3f4a4a44bd1e7f6490f2c5855e5595
srainsdon <srainsdon@nunetnetworks.net>
2013-01-13 15:59:23 -0800

    Added onCommand
    Setup hh-info command

--- src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
+++ src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
@@ -1,5 +1,17 @@
 package com.github.nunetnetworks.HappyHotel;
 
-public class HappyHotel {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class HappyHotel extends JavaPlugin implements Listener{
+	
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("hh-info")){
+    		sender.sendMessage("Happy Hotel Info");
+            return true;
+    	}
+    	return false;
+    }
 }
