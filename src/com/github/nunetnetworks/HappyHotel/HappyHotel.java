d56afa52e4379b7e886a556b55b66a71d666305f
srainsdon <srainsdon@nunetnetworks.net>
2013-01-13 17:24:11 -0800

    onEnable write defualt config

--- src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
+++ src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
@@ -7,6 +7,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HappyHotel extends JavaPlugin implements Listener{
 	
+	public void onEnable() {
+        this.saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+	}
+	
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("hh-info")){
     		sender.sendMessage("Happy Hotel Info");
