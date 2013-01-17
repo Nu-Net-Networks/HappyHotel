15b47f369eeffe509059182d457d35d2bd751fc0
srainsdon <srainsdon@nunetnetworks.net>
2013-01-17 01:15:17 -0800

    Seting up Config file permisions and help command

--- src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
+++ src/com/github/nunetnetworks/HappyHotel/HappyHotel.java
@@ -14,7 +14,9 @@ public class HappyHotel extends JavaPlugin implements Listener{
 	
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("hh-info")){
-    		sender.sendMessage("Happy Hotel Info");
+    		sender.sendMessage("--== Happy Hotel Info ==--");
+    		sender.sendMessage("Base Room Rate: " + this.getConfig().getDouble("BaseRoomRate"));
+    		sender.sendMessage("Membership System Status: " + this.getConfig().getBoolean("membership"));
             return true;
     	}
     	return false;
